c61d610015f7f22b97d9498187d3d257e5ab4408
tsyu12345 <syukut@gmail.com>
2023-08-16 22:38:45 +0900

    FontAwsome導入、フロントのひな型作成

---
+++ Frontend/src/components/FileUploader.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue'
+
+const msg = ref('選択してファイルをアップロードしてください。')
+
+</script>
+
+
+<template>
+    <div class="container">
+        <label for="file-uploader">
+            <input type="file" class="d-none">
+            <i class="fa-solid fa-file fa-10x"></i>
+        </label>
+        <p v-text="msg"></p>
+    </div>
+</template>
+
+
+<style scoped>
+    .container {
+        width: fit-content;
+        border: 3px solid black;
+        padding: 20px;
+        display: flex;
+        justify-content: center; /*左右中央揃え*/
+        align-items: center;     /*上下中央揃え*/
+    }
+
+    label {
+        width: 100%;
+    }
+
+    p, label {
+        text-align: center;
+    }
+</style>
--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,11 +1,20 @@
-import './assets/main.css'
+import "./assets/global.css"
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {faLeaf, faFile} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import "bootstrap/dist/css/bootstrap.min.css"
+
+library.add(faLeaf)
+library.add(faFile)
+
+
 const app = createApp(App)
-
+app.component('fa', FontAwesomeIcon )
 app.use(router)
-
 app.mount('#app')
